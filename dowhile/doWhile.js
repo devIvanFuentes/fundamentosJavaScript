da207bd28de5cdf6144a3025a04f86a19278cd8a
Iván Fuentes <ivanfuentesglez@gmail.com>
2019-03-06 21:17:41 -0600

    Clase ciclo do while

---
+++ dowhile/doWhile.js
@@ -0,0 +1,17 @@
+var contador = 1
+
+const llueve = () => Math.random() < 0.25
+
+do{
+
+	contador++
+
+}while(!llueve())
+
+if(contador === 1 ){
+	console.log(`Fui a ver si llovia ${contador} vez`)
+	
+}else{
+	console.log(`Fui a ver si llovia ${contador} veces`)
+	
+}
