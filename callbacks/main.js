ff28098289676e1a0d409c9bf460756a001b1492
Iván Fuentes <ivanfuentesglez@gmail.com>
2019-05-12 20:57:25 -0500

    Multiples promesas

--- callbacks/main.js
+++ callbacks/main.js
@@ -26,9 +26,22 @@ function onError(id){
 }
 
 obtenerPersonaje(1)
-	.then(function(personaje){
+	.then(personaje => {
 		console.log(`El personaje 1 es ${personaje.name}`)
+		return obtenerPersonaje(2)
 	})
+	.then(personaje =>{
+		console.log(`El personaje 2 es ${personaje.name}`)
+		return obtenerPersonaje(3)
+
+	})
+	.then(personaje =>{
+		console.log(`El personaje 3 es ${personaje.name}`)
+		return obtenerPersonaje(4)
+	})
+	.then(personaje =>{
+		console.log(`El personaje 4 es ${personaje.name}`)
+		return obtenerPersonaje(5)})
 	.catch(onError)
 
 
@@ -40,11 +53,3 @@ obtenerPersonaje(1, function(personaje){
 
 	
 })
-
-
-
-// 
-
-
-
-
