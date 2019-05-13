bc12172feec154ff6abaf9851bc50f4554203449
Iván Fuentes <ivanfuentesglez@gmail.com>
2019-05-12 19:37:44 -0500

    Ordenamiento callbacks

--- callbacks/main.js
+++ callbacks/main.js
@@ -8,16 +8,39 @@ const onPeopleResponse =  function(persona){
 	console.log(`Hola, yo soy ${persona.name}`)
 }
 
-function obtenerPersonaje(id){
+function obtenerPersonaje(id,callback){
+	console.log(id)
 	const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
-	$.get(url, opts,onPeopleResponse)
+	
+	$.get(url, opts,function(persona){
+		console.log(`Hola, yo soy ${persona.name}`)
+
+		if(callback){
+			callback()
+		}
+	})
+
+
+
 }
 
-obtenerPersonaje(1)
+obtenerPersonaje(1, function(){
+	obtenerPersonaje(2, function(){
+		obtenerPersonaje(3,function(){
+			obtenerPersonaje(4,function(){
+				obtenerPersonaje(5,function(){
+					obtenerPersonaje(6,function(){
+						obtenerPersonaje(7)
+					})
+				})
+			})
+		})
+	})
+})
 
-obtenerPersonaje(2)
 
-obtenerPersonaje(3)
+
+// 
 
 
 
